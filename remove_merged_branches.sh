0ca36440b0fbcfeac1c38b1c5f368cdb0fdf7faf
codmw44 <codmw44@gmail.com>
2021-01-29 11:03:10 -0500

    Add support bugfix branches and improving flow

--- remove_merged_branches.sh
+++ remove_merged_branches.sh
@@ -3,18 +3,16 @@
 cd $1
 echo "Git directory: $(pwd)"
 
-
-if [[ "$2" == "delete" ]]; then
-    while true; do
-    read -p "Do you wish to delete and push already mearged branches? (Yes/No)" yn
-    case $yn in
-        [Yy]* ) git branch -r --merged develop | grep -e "feature" | sed "s/origin\///" | xargs -n 1 git push --delete origin; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-    done
-    
-else
-    echo "Show branches to delete. Run script with "delete" argument for delete"
-    git branch -r --merged develop | grep -e "feature" | sed "s/origin\///" | xargs -n 1
-fi
+echo "Showing branches to delete"
+git branch -r --merged develop | grep -e "feature\|bugfix" | sed "s/origin\///" | xargs -n 1
+while true; do
+  read -p "Do you wish to delete and push those mearged branches? (Yes/No)" yn
+  case $yn in
+  [Yy]*)
+    git branch -r --merged develop | grep -e "feature\|bugfix" | sed "s/origin\///" | xargs -n 1 git push --delete origin
+    break
+    ;;
+  [Nn]*) exit ;;
+  *) echo "Please answer yes or no." ;;
+  esac
+done
